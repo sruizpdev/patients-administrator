ce7c82196e7196800e303186070c701312bca299
Sergio Ruiz <sergiotto@outlook.com>
2020-05-25 18:37:36 +0200

    finishing the project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
-  const [citas, guardarCitas] = useState([]);
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas, citasIniciales]);
+
   const crearCita = (cita) => {
     guardarCitas([...citas, cita]);
   };
 
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
       <div className="container">
-        <div className="one-half column">
-          <Formulario crearCita={crearCita} />
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
         </div>
-        <div className="one-half column">2</div>
       </div>
     </Fragment>
   );
