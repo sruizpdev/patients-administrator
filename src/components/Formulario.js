1941dcf030aef0ebd333eb7d861a5031921ba59f
Sergio Ruiz <sergiotto@outlook.com>
2020-05-25 16:21:05 +0200

    form

---
+++ src/components/Formulario.js
@@ -0,0 +1,73 @@
+import React, { Fragment, useState } from "react";
+
+const Formlario = () => {
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  return (
+    <Fragment>
+      <h2>Crear cita</h2>
+      <form>
+        <label>Nombre mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+        <label>Nombre del dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre dueño de la mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          propietario={fecha}
+        />
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+        <label>Síntomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Agregar cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formlario;
